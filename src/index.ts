298a7fa6c7b948065aed5909751cb9b0ba3c9e2d
Robert Brunson <siinspam@gmail.com>
2022-05-15 00:50:59 -0400

    added types to the index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export * from './lib/client/WaniClient';
+
+//types
+export * from './lib/types/WKSubject';
+export * from './lib/types/WKSrs';
+export * from './lib/types/WKUser';
+export * from './lib/types/WKAssignment';
+export * from './lib/types/WKReset';
+export * from './lib/types/WKResponse';
